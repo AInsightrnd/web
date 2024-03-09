75928cbcb0364374f793bf290c1b05fc7e894abe
hyungkyulee <hyungkyu@deepeyes.co.uk>
2024-03-09 00:05:36 +0000

    company history updated

--- src/components/CompanyHistory.tsx
+++ src/components/CompanyHistory.tsx
@@ -15,14 +15,14 @@ const activity = [
   },
   { id: 2, type: 'month',
     history: {
-      name: '제품출시 - AICiTi',
-      description: 'AICiTi program version 4.1 출시',
+      name: 'AICiTi program version 4.1 출시',
+      description: '',
     }, 
     date: '2021-09-10',
   },
   { id: 3, type: 'new-year',
     history: {
-      name: '국내특허 등록',
+      name: '국내 특허 등록',
       description:
         `자연 두부 위치에서 촬영된 3차원 CBCT 영상에서 기계  학습 기반  치아교정 진단을 위한 
         두부 계측 파라미터 도출방법 – 국내 특허 등록 완료`,
@@ -31,7 +31,7 @@ const activity = [
   },
   { id: 4, type: 'month',
     history: {
-      name: 'PCT출원 완료',
+      name: 'PCT 출원 완료',
       description:
         `자연 두부 위치에서 촬영된 3차원 CBCT 영상에서 기계  학습 기반  치아교정 진단을 위한 
         두부 계측 파라미터 도출방법 – 국내 특허 등록 완료`,
@@ -40,36 +40,34 @@ const activity = [
   },
   { id: 5, type: 'month',
     history: {
-      name: '논문 게재',
+      name: '프로그램 검증 논문 게재',
       description:
       `Computer Methods and Programs in Biomedicine에 “Automated analysis of three-dimensional CBCT
-      images taken in natural head position that combinesfacial profile processing and multiple deep-learning models” 프로그램 검증 논문 게재.`,
+      images taken in natural head position that combinesfacial profile processing and multiple deep-learning models"`,
     }, 
     date: '2022-09-08',
   },
   { id: 6, type: 'new-year',
     history: {
-      name: '국내출원 완료',
+      name: '국내 출원 완료',
       description:
         `자연 두부 위치에서 촬영된 엑스레이(X-ray) 이미지를 이용한 기계학습 기반 치아 교정 진단을 위한 
-        두부계측 파라미터 도출방법 – 국내 출원 완료`,
+        두부계측 파라미터 도출방법`,
     }, 
     date: '2023-03-23',
   },
   { id: 7, type: 'month',
     history: {
-      name: '사업과제 선정',
-      description:
-        `산자부 주관 중견 중소 기업 상생형 혁신 도약 사업 과제 선정
-        (주관기업 – 바텍사, 총 사업비 – 39억, 
-        주식회사 아인사이트 – 6억)`,
+      name: '산자부 사업 과제 선정',
+      description: '산자부 주관 중견 중소 기업 상생형 혁신 도약 사업 과제 선정',
+      note: '(주관기업 – 바텍사, 총 사업비 – 39억, 주식회사 아인사이트 – 6억)',
     }, 
     date: '2023-04-25',
   },
   { id: 8, type: 'month',
     history: {
-      name: '제품출시 - AICiTi',
-      description: `AICiTi program version 7.7 출시`,
+      name: 'AICiTi program version 7.7 출시',
+      description: ``,
     }, 
     date: '2023-06-30',
   },
@@ -93,7 +91,7 @@ export function CompanyHistory() {
           </h1>
         </div>
 
-        <ul role="list" className="mt-12 mx-10 md:mx-20 lg:mx-60 space-y-6">
+        <ul role="list" className="mt-12 mx-10 md:mx-20 lg:mx-40 xl:w-1/2 xl:mx-auto space-y-6">
           {activity.map((activityItem, activityItemIdx) => (
             <li key={activityItem.id} className="relative flex gap-x-4">
               <div className="mt-1 text-ainblue-600 text-lg font-bold" 
@@ -119,9 +117,10 @@ export function CompanyHistory() {
                   <div className="h-2 w-2 rounded-full bg-gray-100 ring-1 ring-gray-300" />
                 )}
               </div>
-              <p className="flex-auto mt-2 pr-10 text-xs leading-5 text-gray-500">
+              <p className="flex-auto mt-2 pr-20 md:pr-20 lg:pr-40 lx:pr-60 text-xs leading-5 text-gray-500">
                 <div className="font-bold text-sm text-gray-50">{activityItem.history.name}</div> 
-                <div className="font-medium text-gray-200">{activityItem.history.description}</div> 
+                <div className="font-medium italic text-gray-200">{activityItem.history.description}</div> 
+                {activityItem.history.note && <div className="font-medium italic text-gray-200">{activityItem.history.note}</div>}
               </p>
               <time className="flex-none py-0.5 text-xs leading-5 text-gray-500">
                 {activityItem.date}
