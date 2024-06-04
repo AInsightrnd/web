75b0b785f15f449203186bb87f4930959ed05398
j-brem <jang.brem@gmail.com>
2024-06-04 12:51:39 +0900

    fields text margin edited

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -8,7 +8,7 @@ function Label({ id, children }: { id: string; children: React.ReactNode }) {
   return (
     <label
       htmlFor={id}
-      className="mb-2 block text-sm font-semibold text-gray 700"
+      className="mb-1 block text-sm font-semibold text-gray-700"
     >
       {children}
     </label>
