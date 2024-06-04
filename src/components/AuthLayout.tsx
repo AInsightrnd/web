c72beec74b39c4691933cf844511bec099267fbc
j-brem <jang.brem@gmail.com>
2024-06-04 12:28:41 +0900

    Sign-in page UI edited

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-import { CirclesBackground } from '@/components/CirclesBackground'
+
 import { Logo } from '@/components/Logo'
 import { ImageLogo } from './ImageLogo'
 
@@ -21,19 +21,15 @@ export function AuthLayout({
           <ImageLogo className='mx-auto mt-4 h-10 w-auto' />
         </Link>
         <div className="relative mt-12 sm:mt-16">
-          <CirclesBackground
-            width="1090"
-            height="1090"
-            className="absolute -top-7 left-1/2 -z-10 h-[788px] -translate-x-1/2 stroke-gray-300/30 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:-top-9 sm:h-auto"
-          />
-          <h1 className="text-center text-2xl font-medium tracking-tight text-gray-900">
+
+          <h1 className="text-center text-2xl font-medium tracking-tight text-gray-800">
             {title}
           </h1>
           {subtitle && (
-            <p className="mt-3 text-center text-lg text-gray-600">{subtitle}</p>
+            <p className="mt-1 text-center font-medium text-lg text-gray-500">{subtitle}</p>
           )}
         </div>
-        <div className="-mx-4 mt-10 flex-auto bg-white px-4 py-10 shadow-2xl shadow-gray-900/10 sm:mx-0 sm:flex-none sm:rounded-5xl sm:p-24">
+        <div className="-mx-4 mt-8 flex-auto bg-white px-4 py-10 shadow-2xl shadow-gray-900/10 sm:mx-0 sm:flex-none sm:rounded-sm sm:p-24">
           {children}
         </div>
       </div>
